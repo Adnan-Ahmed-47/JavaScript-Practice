74629d7a97592d82fc1bcbe7b5838c50a42d6210
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-10-19 20:39:42 +0530

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+// var str = "typescript"
+// var str1 = "livescript"
+
+// #################################################  STRING METHODS ###############################################################
+// console.log(str.length)
+// console.log(str.toUpperCase())  
+// console.log(str.toLowerCase()) 
+// console.log(str.substr(0,4)) 
+// console.log(str.split('p'))
+// console.log(str.trim())
+// console.log(str.includes('x'))
+// console.log(str.replace('type', 'java'))
+// console.log(str.charAt(4))
+// console.log(str.concat(" " ,str1))
+// console.log(str.startsWith('z'))
+// console.log(str.endsWith('t'))
+
+
+// ################################################# MATH OBJECT ###############################################################
+
+var a = 4.2389679
+// console.log(Math.round(a))
+console.log(Math.floor(a))
