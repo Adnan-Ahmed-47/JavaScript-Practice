2c711bb69c711523faaf1bdbebebf454ccc27d58
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-10-26 21:57:18 +0530

    done

--- arrayEx1.js
+++ arrayEx1.js
@@ -25,16 +25,37 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------------
 
 var itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
-console.log(itCompanies)
-
+// console.log(itCompanies)
+    // ---------------------------------------------------------
 // console.log("Number of Companies : " , itCompanies.length)
-
+    // ---------------------------------------------------------
 // console.log("First Company : " ,itCompanies[0])
 // console.log("Middle Company : " , itCompanies[Math.floor(itCompanies.length/2)])
 // console.log("Last Company : " , itCompanies[itCompanies.length-1])
-
-first = 'itCompanies[0]'
-console.log(first)
+    // ---------------------------------------------------------
+// var arr1 =[]
+// arr1.push( itCompanies[0].toUpperCase())
+// arr1.push( itCompanies[1].toUpperCase())
+// arr1.push( itCompanies[2].toUpperCase())
+// arr1.push( itCompanies[3].toUpperCase())
+// arr1.push( itCompanies[4].toUpperCase())
+// arr1.push( itCompanies[5].toUpperCase())
+// arr1.push( itCompanies[6].toUpperCase())
+// console.log(arr1)
+    // ---------------------------------------------------------
+// var store = itCompanies.pop()
+// console.log(itCompanies)
+// console.log(itCompanies.join(), "and" , store, "are big IT companies.")
+    // ---------------------------------------------------------
+var chComp = 'Microsoft'
+for(var i=1; i<=itCompanies.length; i++){
+    if(chComp == itCompanies[i]){
+        console.log(itCompanies[i])
+    }
+    else{
+        console.log("Company is not found")
+    }
+}                       
 
 
 
