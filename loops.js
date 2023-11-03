ecfee90b78fe6be6a5de6569a8365940764d7a3b
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-11-03 21:58:14 +0530

    done

--- loops.js
+++ loops.js
@@ -21,6 +21,7 @@
 // console.log("sum of all numbers : " , sum)
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------
+
 // var evenSum = 0
 // var oddSum = 0
 // for(var i=1; i<=100; i++){
@@ -37,3 +38,61 @@
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------
 
+// var evenSum = 0
+// var oddSum = 0
+
+// for(var i=1; i<=100; i++){
+//     if(i%2 == 0){
+//         evenSum += i
+//     }
+//     else{
+//         oddSum += i
+//     }
+// }
+// var arr = []
+// arr.push(evenSum)
+// arr.push(oddSum)
+// console.log(arr)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+// var arr = ['ALBANIA', 'BOLIVIA', 'CANADA', 'DENMARK', 'ETHIOPIA', 'FINLAND', 'GERMANY', 'HUNGARY', 'IRELAND', 'JAPAN', 'KENYA']
+
+// var newArr = []
+// for(var i=0; i<=arr.length-1; i++){
+//     var val = arr[i].length
+//     newArr.push(val)
+// }
+// console.log(newArr)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+// var arr = ['ALBANIA', 'BOLIVIA', 'CANADA', 'DENMARK', 'ETHIOPIA', 'FINLAND', 'GERMANY', 'HUNGARY', 'IRELAND', 'JAPAN', 'KENYA']
+
+// var biggestNum = 0
+// var country;
+// for(var i=0; i<=arr.length-1; i++){
+//     var len = arr[i].length
+
+//     if(len > biggestNum){
+//         biggestNum = len;
+//         country=arr[i]   
+//     }
+
+// }
+// console.log(country)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+var arr = ['ALBANIA', 'BOLIVIA', 'CANADA', 'DENMARK', 'ETHIOPIA', 'FINLAND', 'GERMANY', 'HUNGARY', 'IRELAND', 'JAPAN', 'KENYA']
+
+var country
+for(var i=0; i<=arr.length-1; i++){
+    var len = arr[i].length
+
+    if(len == 5){
+        country = arr[i]
+    }
+}
+
+console.log(country)
