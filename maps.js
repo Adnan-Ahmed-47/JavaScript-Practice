d39d8e1ef8a328f29029ac20bc8f535100a73eaa
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-11-06 15:15:38 +0530

    modified

--- maps.js
+++ maps.js
@@ -37,3 +37,41 @@
 // })
 
 // console.log(newArr)
+
+// _______________________________________________________________________________________________________________________________________
+
+// var arr = ['html', 'css', 'js']
+// var newArr = arr.map((arr)=>arr.toUpperCase())
+// console.log(newArr)
+
+// ---------------------------------------------------------------------------------------------
+// var sqNum = [1, 2, 3, 4, 5]
+// var newNum = sqNum.map((sqNum)=> sqNum*sqNum)
+// console.log(newNum)
+
+// TRIED BY ME 
+// var newNum = []
+// function squareNums(sqNum){
+//     for(var i=0; i<=sqNum.length-1; i++){
+//         // var final = sqNum[i]*sqNum[i]
+//         var final = i*i
+//         newNum.push(final)
+//     }
+//     // return newNum
+//     console.log(newNum)
+// }
+// squareNums([1, 2, ,3 ,4 , 5])
+// // console.log(squareNums([1, 2, ,3 ,4 , 5]))
+// ------------------------------------------------------------------------------------------------
+
+// var students = [{name:'Adnan', age:23}, {name:'Meraj', age:25}, {name:'Mateen', age:24}, {name:'Sameer', age:26}]
+// var studNames = students.map((students)=> (students.name))
+// console.log(studNames)
+
+// ------------------------------------------------------------------------------------------------
+// var temp = [98, 87, 90, 94, 96] 
+// // console.log(temp.join('C '))
+// var newArr = temp.map((temp)=> (temp*9/5)+32+ 'F')
+// console.log(newArr)
+
+// ------------------------------------------------------------------------------------------------
