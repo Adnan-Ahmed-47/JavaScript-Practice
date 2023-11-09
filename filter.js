4b6fb16e0982e0211bfec2bdb7a52f325c43c36f
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-11-09 19:18:28 +0530

    modified

--- filter.js
+++ filter.js
@@ -5,17 +5,22 @@
 // console.log(newArr)
 
 // ------------------------------------------------------------------------------------------------
-var country = ['INDIA', 'PALESTINE', 'SAUDI', 'AMERICA', 'AUSTRALIA', 'NEPAL']
 
-var fcountry = country.filter((item) => {
-    // var len = country[item].length
-    // if(len < 6){
-    //     console.log(country.item)
-    // }
-    // return country.item
-})
+// var country = ['INDIA', 'PALESTINE', 'SAUDI', 'AMERICA', 'AUSTRALIA', 'NEPAL']
 
-console.log(fcountry)
+// var fcountry = country.filter((item) => {
+//     // var len = country[item].length
+//     if(item.length < 6){
+//         // var len = fcountry.push(item)
+//         // console.log(item)
+//         // fcountry.push(country[item])
+//         return item
+//     }
+//     // return country.item
+   
+// })
+
+// console.log(fcountry)
 
 
 
@@ -41,4 +46,48 @@ console.log(fcountry)
 // }
 // console.log(newArr)
 
+// ------------------------------------------------------------------------------------------------
 
+// var arr = ['ETHIOPIA', 'INDIA', 'EUROPE', 'AMERICA', 'AUSTRALIA', 'ENGLAND']
+
+// var newArr = arr.filter((elems)=> {
+//     return elems.startsWith('E')
+// })
+// console.log(newArr)
+
+// ------------------------------------------------------------------------------------------------
+
+// var obj = [
+//     {product:'Shoes', price: 8},
+//     {product:'Watch', price: 12},
+//     {product:'Shirt', price: 9},
+//     {product:'Bag', price: 11},
+//     {product:'Coke', price: 5}
+
+// ]
+
+// var arr = obj.filter((elems)=> {
+//     if(elems.price < 10){
+//         return elems
+//     }
+// })
+
+// console.log(arr)
+
+// ------------------------------------------------------------------------------------------------
+
+var empDet = [
+    {name:'Mohan', salary:28000},
+    {name:'Raj', salary:56000},
+    {name:'Daniel', salary:52000},
+    {name:'Raheem', salary:78000},
+    {name:'Rajesh', salary:12000},
+    {name:'Mohit', salary:50000},
+]
+
+var employees = empDet.filter((item)=> {
+    if(item.salary >= 50000){
+        return item
+    }
+})
+console.log(employees)
---
+++ reduce.js
@@ -0,0 +1,19 @@
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// var arr1 = []
+// var newArr = arr.reduce((p,c)=> {
+//     p = p+c
+//     // arr1.push(p)
+//     return p
+// },0)
+// // newArr.push(p)
+// arr1.push(newArr)
+// console.log(arr1)
+
+// ------------------------------------------------------------------------------------------------
+
+var arr = ['Harry', 'is', 'the', 'best', 'coder']
+
+var newArr = arr.reduce((p,c) => {
+    
+},0)
