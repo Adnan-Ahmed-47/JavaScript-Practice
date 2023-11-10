ca59454ad3900b2eaffb0870964d48ba38c10934
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-11-10 21:58:45 +0530

    modified

---
+++ Toggle Project/DomPractise/domPrac.js
@@ -0,0 +1,8 @@
+// var target = document.querySelectorAll('.key')
+// console.log(target[4].textContent)
+
+// var body=document.querySelector("body");
+// var create = document.createElement('div');
+// create.textContent="Created";
+// body.append(create)
+// console.log(create.textContent)
