ef3301af3f2cafae1ead2031557c4514b2aaa757
Adnan-Ahmed-47 <mdadnaan147@gmail.com>
2023-11-12 19:04:28 +0530

    modified

---
+++ ToDoList Project/script.js
@@ -0,0 +1,26 @@
+// console.log('1' - - '1')
+// var a = '1' - - '1' 
+// console.log(typeof a)
+// console.log(a)
+
+
+// console.log(1 + '2' + '2')
+
+
+// (function(){
+//     var a=b=5;
+// })()
+// console.log(b);
+
+
+// var a=1;
+// function b(){
+//     a=10;
+//     return;
+//     function a() {}
+
+// }
+// console.log(b())
+
+
+
--- Toggle Project/DomPractise/domPrac.js
+++ Toggle Project/DomPractise/domPrac.js
@@ -1,8 +1,119 @@
 // var target = document.querySelectorAll('.key')
 // console.log(target[4].textContent)
+// console.log(target.textContent)    // Have a doubt in this line
 
-// var body=document.querySelector("body");
+
+// var body = document.querySelector("body");
 // var create = document.createElement('div');
 // create.textContent="Created";
 // body.append(create)
 // console.log(create.textContent)
+
+
+
+
+// create.setAttribute('class', 'cl1')
+// create.setAttribute('id', 'id1')
+
+// console.log(create)
+
+// ______________________________________________________________________________________________
+
+// Event Listeners
+
+let btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', function (e) {
+
+//     // e.target.value = "BUTTON CLICKED"
+//     console.log("button clicked")
+// })
+
+// btn.addEventListener('dblclick', function (e) {
+
+//     // e.target.value = "BUTTON CLICKED"
+//     console.log("button clicked")
+// })
+
+// let movAct
+
+// let box = document.querySelector('.box');
+// box.addEventListener('mouseenter', (e) => {
+//     // console.log("mouse enter")\
+//     box.style.backgroundColor = "red"
+//     // box.setAttribute('id','bx')
+// })
+
+
+// box.addEventListener('mouseleave', (e) => {
+//     // console.log(e)
+//     console.log("mouse leave")
+//     box.style.backgroundColor = "black"
+// })
+
+// box.addEventListener('mousemove', (e) => {
+//     // console.log(e.clientX)
+//     // movAct = e.timeStamp
+//     // console.log("mouse move")
+//     // box.style.backgroundColor = "black"
+// })
+
+
+
+// box.addEventListener('mouseover', (e) => {
+//     // console.log(e.clientX)
+//     // movAct = e.timeStamp
+//     // console.log("mouse over")
+//     // box.style.backgroundColor = "black"
+// })
+// // console.log(movAct)
+
+// box.addEventListener('mouseout', (e) => {
+//     // console.log(e.clientX)
+//     // movAct = e.timeStamp
+//     console.log("mouse out")
+//     // box.style.backgroundColor = "black"
+// })
+
+
+// let username = document.querySelector('input[placeholder=username]')
+// let password = document.querySelector('input[placeholder=password]')
+
+
+// username.addEventListener('input', function(e){
+//     console.log(e.target.value)
+// })
+
+
+// username.addEventListener('change', function (e) {
+//     console.log(e.target.value)
+// })
+
+// username.addEventListener('blur', function (e) {
+//     console.log(e)
+// })
+
+
+// username.addEventListener('focus', function (e) {
+       
+// })
+
+
+
+// username.addEventListener('keydown', function (e) {
+//     console.log(e)
+//     console.log(e.target.value)
+// })
+// let inpVal
+// username.addEventListener('keyup', function (e) {
+//     inpVal = e.target.value
+// console.log(inpVal)
+// if(inpVal.length<=3){
+//     username.style.border = "1px solid red"
+// }else if(inpVal.length > 3){
+//     username.style.border = "1px solid blue"
+
+// }
+
+// })
+ 
